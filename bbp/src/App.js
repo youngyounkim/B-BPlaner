b18917742c88fc641ae3cef9313d142a4f9edf73
YU-WON <shallwedance0419@gmail.com>
2021-04-04 00:18:27 +0900

    Commit Test

--- bbp/src/App.js
+++ bbp/src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      test
-    </div>
-  );
+  return <div className="App">test test2</div>;
 }
 
 export default App;
