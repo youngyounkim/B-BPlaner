5d95bfff58468d295c778d63a43a88fa6303cc31
rjc1704 <rjc1704@gmail.com>
2021-04-05 00:30:08 +0900

    Color Picker 테스트를 위해 App.js 에 코드 추가

--- bbp/src/App.js
+++ bbp/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-
+import { SketchPicker } from 'react-color'
 
 import TargetList from "./component/TargetList"  // 테스트용 입니다. - 민석
 
 function App() {
+  const handleColor = function (color) { // 테스트용 - 제천
+    console.log('color: ', color.hex);
+  }
   return (
     <>
       <TargetList/> {/* 테스트용 입니다. -- 민석 */}
+      <SketchPicker onChangeComplete={handleColor}/> {/* 테스트용 제천 */}
     </>
 
   )
