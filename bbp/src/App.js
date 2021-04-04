6f6e9d0118fc999967c1f65eb081e9ca4c9e9966
rjc1704 <rjc1704@gmail.com>
2021-04-04 11:31:33 +0900

    James Test1

--- bbp/src/App.js
+++ bbp/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">test test2</div>;
+  return (
+    <>
+    <div className="App">test test2</div>
+    <div className="App">Jecheon test3 :)</div>
+    </>
+
+  )
 }
 
 export default App;
