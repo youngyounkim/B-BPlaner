59570dba7abb450a012496aa771f34236b657332
rjc1704 <rjc1704@gmail.com>
2021-04-06 15:37:35 +0900

    package-lock.json and TargetList to pull from remote repo

--- bbp/src/component/TargetList.js
+++ bbp/src/component/TargetList.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState , useMemo } from 'react';
 import ReadTargetList from './ReadTargetList'
-// import AddTarget from './AddTarget'
+import AddTarget from './AddTarget'
 import loadingImg from '../images/loading.gif'
 // import dummyData from '../data/targetData.json'
-import AddTarget from './AddTarget.js'
 
+import dummy from '../redux/dummy.json'
 
 function TargetList (props) {
     const [target , setTarget] = useState(null)
 
-    // useEffect(()=>{
-    //     setTimeout(()=>setTarget(dummyData),3000)
-    // })
+    useEffect(()=>{
+        setTimeout(()=>setTarget(dummy.target),1000)
+    })
 
     return (
         <div id = 'TargetList'>
@@ -22,7 +22,7 @@ function TargetList (props) {
             : 
             <div>
                 <ReadTargetList id = 'ReadTargetList' target = {target} />
-                {/* <AddTarget setTarget={setTarget}/>  */}
+                {/* <AddTarget />  */}
               </div>
             }
         </div>
