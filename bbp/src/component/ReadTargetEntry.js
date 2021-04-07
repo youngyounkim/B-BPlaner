c31ab673beab743520ce2eb732ca1568012e1620
YU-WON <shallwedance0419@gmail.com>
2021-04-08 01:18:05 +0900

    [Target][ReadTargetEntry]ReadTargetEntry 1차 코드 추가

--- bbp/src/component/ReadTargetEntry.js
+++ bbp/src/component/ReadTargetEntry.js
@@ -1,6 +1,17 @@
+import React from "react";
 
-function ReadTargetEntry () {
-    return <div>dummy</div>
+function ReadTargetEntry({ target }) {
+  return (
+    <div className="readTargetEntry">
+      <div className="mainEntry">
+        <div className="mainEntryName">{target.name}</div>
+        <div className="mainEntryColor">{target.color}</div>
+      </div>
+      <div className="subEntry">
+        <div className="activitesName">{target.activities.name}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ReadTargetEntry;
