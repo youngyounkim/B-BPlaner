adbbddc70040c63f65f8b8d374f918b454243278
YU-WON <shallwedance0419@gmail.com>
2021-04-22 00:09:38 +0900

    [Target][ReadTargetEntry]현재까지의 작업물

--- bbp/src/component/TodoList/TargetList/ReadTargetList/ReadTargetEntry/ReadTargetEntry.js
+++ bbp/src/component/TodoList/TargetList/ReadTargetList/ReadTargetEntry/ReadTargetEntry.js
@@ -1,21 +1,105 @@
-import React from "react";
-import ReadActivityList from "./ReadActivityList/ReadActivityList"
-// 확인했습니다~
-function ReadTargetEntry({ name, color, activities }) {
+import { useState, useRef } from "react";
+import { CirclePicker } from "react-color";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  changeTargetColor,
+  addTarget,
+} from "../../../../../redux/actions/actions";
+import ColorPicker from "../../ColorPicker";
+import reactCSS from "reactcss";
+import ReadActivityList from "../ReadTargetEntry/ReadActivityList/ReadActivityList";
+
+// 테스트 /////////////////////////////////////////////////////////
+
+const ReadTargetEntry = ({ id, name, color, activities }) => {
+  const state = useSelector((state) => {
+    return state.targetReducer;
+  });
+  const dispatch = useDispatch();
+  // const targetInput = useRef();
+
+  const targetName = useState(null);
+
+  // const [targetColor, setTargetColor] = useState({
+  //   r: "241",
+  //   g: "112",
+  //   b: "19",
+  //   a: "1",
+  // });
+
+  // const handleColorPicker = function () {
+  //   if (callFrom === "ReadTargetEntry") {
+  //     dispatch(changeTargetColor(id, targetColor));
+  //   }
+  // };
+
+  // const handleChangeColor = function (color) {
+  //   setTargetColor(color.rgb);
+  //   if (!targetInput) {
+  //     targetInput.current.focus();
+  //   }
+  // };
+
+  const handleTarget = function (e) {
+    e.preventDefault();
+    dispatch(
+      addTarget(
+        state.targetCnt,
+        targetName,
+        `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`
+      )
+    );
+  };
+
+  const styles = reactCSS({
+    default: {
+      color: {
+        width: "100%",
+        height: "20px",
+        borderRadius: "1px",
+        background: `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`,
+      },
+      swatch: {
+        padding: "3px",
+        background: "#fff",
+        borderRadius: "1px",
+        boxShadow: "0 0 0 1px rgba(0,0,0,.1)",
+        display: "inline-block",
+        cursor: "pointer",
+        width: "20px",
+      },
+      popover: {
+        position: "absolute",
+        zIndex: "2",
+        backgroundColor: "#fafafa",
+        padding: "10px",
+        border: "1px solid #212121",
+        borderRadius: "5px",
+      },
+    },
+  });
+
   return (
-    <div className="readTargetEntry">
+    <>
       <div className="mainEntry">
-        <div className="mainEntryName">{name}</div>
-        <div className="mainEntryColor">{color}</div>
+        <form onSubmit={handleTarget} className="MainEntryName" key={id}>
+          {name}
+        </form>
+        <div className="ColorEntry">
+          <ColorPicker
+            id={id}
+            targetColor={color}
+            swatchStyle={styles.swatch}
+            colorStyle={styles.color}
+            popoverStyle={styles.popover}
+            callFrom={"ReadTragetEntry"}
+            // handleChangeColor={handleChangeColor}
+          />
+        </div>
       </div>
-      <div className="subEntry">
-        {<ReadActivityList id = {1}/>}
-        {/* {activities.map((el) => {
-          return <div className="activitesName">{el.name}</div>;
-        })} */}
-      </div>
-    </div>
+      <div className="SubEntry">{<ReadActivityList id={1} />}</div>
+    </>
   );
-}
+};
 
 export default ReadTargetEntry;
